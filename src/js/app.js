02dd7cc94565392e9cb053254d9c77531775adaa
Molodoy3 <artem.nikonov.2005@bk.ru>
2023-03-13 14:33:32 +0500

    lalala

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/* import Swiper, { Navigation, Pagination } from 'swiper'; //импорт свайпера */
+ import {delegationClick} from './modules/script.js';
+
+window.addEventListener("load", windowLoad);
+function windowLoad() {
+    delegationClick();
+}
